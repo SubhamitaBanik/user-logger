e43c7ee785a8d8b30b01eb522903220b05d428d7
Subhamita Banik <138712314+SubhamitaBanik@users.noreply.github.com>
2025-06-30 22:41:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = prompt("Enter your name:");
+let age = prompt("Enter your age:");
+
+if (age < 18) {
+  console.log("Hi " + name + ", you're a minor.");
+} else {
+  console.log("Welcome " + name + ", you're an adult.");
+}
